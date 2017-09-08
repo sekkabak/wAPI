c2f0382c41f7b0affc4cda671625193897505326
sekka <sekkabak@gmail.com>
2017-09-08 20:06:45 +0200

    samples

--- app.php
+++ app.php
@@ -7,7 +7,18 @@
 require_once __DIR__ . '/Core.php';
 
 $api = new Core();
+
+// single
+//$result = $api->decodeOneCode('28-200', '41-203', '30-001', '71-001');
+
+// array
 $result = $api->decodeArrayOfCodes(['28-200', '41-203', '30-001', '71-001']);
+
+// json
+// WIP
+
 var_dump($result);
 echo '<br>';
+
+// czas wykonania
 echo $api->getExecTime();
